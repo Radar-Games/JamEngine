c4d97201725ac971941230b19bfea8c0c0934587
Radar-Games <radargames.jc@gmail.com>
2017-11-03 14:08:41 +0800

    Changed Order of Abstract Methods

--- BigDogThomas/src/com/thomas/engine/AbstractGame.java
+++ BigDogThomas/src/com/thomas/engine/AbstractGame.java
@@ -2,7 +2,7 @@ package com.thomas.engine;
 
 public abstract class AbstractGame 
 {
+	public abstract void initialize(GameContainer gc);
 	public abstract void update(GameContainer gc, float dt);
 	public abstract void render(GameContainer gc, Renderer r);
-	public abstract void initialize(GameContainer gc);
 }
